86105e13dcdbccade456cd37eab8c64123c04c2c
Rachesk <juan.godoy.klenner@gmail.com>
2024-09-15 17:52:03 -0300

    0.1

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  username: string = '';
+  password: string = '';
+  message: string = 'Bienvenido';
 
-  constructor() {}
+  constructor(
+    private router: Router
+  ){
+
+  }
+
+  validateLogin() {
+    if (this.username === 'admin' && this.password === '1234') {
+      this.message = 'Acceso correcto';
+      this.router.navigate(['/index']);
+    } else {
+      this.message = 'Acceso incorrecto';
+    }
+  }
 
 }
