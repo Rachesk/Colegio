6d8bc085cefad34ca0a17bfc3f26f8880f5ac1cc
Rachesk <juan.godoy.klenner@gmail.com>
2024-09-21 13:09:31 -0300

    1.8

--- src/app/menu-home/menu-home.page.ts
+++ src/app/menu-home/menu-home.page.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate, keyframes } from '@angular/animations'; 
 
 @Component({
   selector: 'app-menu-home',
   templateUrl: './menu-home.page.html',
   styleUrls: ['./menu-home.page.scss'],
+  
 })
 export class MenuHomePage implements OnInit {
 
