c491d695cad3f11f0833d2bedfec306e99713574
Pipppox <piporibot@gmail.com>
2024-09-21 04:35:50 -0300

    mientrasQR

--- src/app/asistencia-generador/asistencia-generador.module.ts
+++ src/app/asistencia-generador/asistencia-generador.module.ts
@@ -7,13 +7,15 @@ import { IonicModule } from '@ionic/angular';
 import { AsistenciaGeneradorPageRoutingModule } from './asistencia-generador-routing.module';
 
 import { AsistenciaGeneradorPage } from './asistencia-generador.page';
+import { QRCodeModule } from 'angularx-qrcode';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    AsistenciaGeneradorPageRoutingModule
+    AsistenciaGeneradorPageRoutingModule,
+    QRCodeModule
   ],
   declarations: [AsistenciaGeneradorPage]
 })
--- src/app/asistencia-generador/asistencia-generador.page.ts
+++ src/app/asistencia-generador/asistencia-generador.page.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-asistencia-generador',
   templateUrl: './asistencia-generador.page.html',
   styleUrls: ['./asistencia-generador.page.scss'],
 })
-export class AsistenciaGeneradorPage implements OnInit {
+export class AsistenciaGeneradorPage {
 
-  constructor() { }
-
-  ngOnInit() {
+  public qrData: string = '';
+  constructor() { 
+    this.generateRandomQR();
   }
 
+  generateRandomQR() {
+    this.qrData = this.generateRandomString(10); 
+  }
+  
+  generateRandomString(length: number): string {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+    const charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+  }
 }
