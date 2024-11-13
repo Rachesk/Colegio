97ae855375efb6c7b44e192dbd8acb63a6900e9f
Pipppox <piporibot@gmail.com>
2024-11-13 20:29:02 -0300

    cambios temporales en registrar qr

--- src/app/registrar-qr/registrar-qr.page.ts
+++ src/app/registrar-qr/registrar-qr.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { LoadingController } from '@ionic/angular';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
 
 @Component({
   selector: 'app-registrar-qr',
@@ -24,21 +25,29 @@ export class RegistrarQrPage implements OnInit {
     },
   ];
 
-  constructor(private loadingController: LoadingController) {
-    
-   }
+  constructor(
+    private loadingController: LoadingController,
+    private barcodeScanner: BarcodeScanner
+  ) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
-  async loading2(){
+  async loading2() {
     const loading = await this.loadingController.create({
       message: 'Cargando...',
       spinner: 'bubbles',
       duration: 500
     });
-    
     await loading.present();
   }
 
+  // Nueva función para abrir la cámara y escanear un código QR
+  scanQRCode() {
+    this.barcodeScanner.scan().then(barcodeData => {
+      console.log('Código QR escaneado:', barcodeData.text);
+      // Aquí puedes manejar el resultado escaneado
+    }).catch(err => {
+      console.error('Error al escanear el código QR:', err);
+    });
+  }
 }
