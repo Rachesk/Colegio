60b865e473aea393fd6cfd0f441bb7f74476cdcb
Pipppox <piporibot@gmail.com>
2024-11-19 23:48:47 -0300

    cambionuevo

--- src/app/registrar-qr/registrar-qr.page.ts
+++ src/app/registrar-qr/registrar-qr.page.ts
@@ -1,22 +1,53 @@
-import { Component, OnInit } from '@angular/core';
-import { LoadingController, ToastController } from '@ionic/angular';
-import { CapacitorBarcodeScanner, CapacitorBarcodeScannerTypeHint, CapacitorBarcodeScannerTypeHintALLOption } from '@capacitor/barcode-scanner';
+import { Component } from '@angular/core';
+import { Camera, CameraResultType } from '@capacitor/camera';
+import jsQR from 'jsqr';
 
 @Component({
   selector: 'app-registrar-qr',
   templateUrl: './registrar-qr.page.html',
   styleUrls: ['./registrar-qr.page.scss'],
 })
-export class RegistrarQrPage  {
-  result: string = ''
+export class RegistrarQrPage {
+  result: string = '';
 
   constructor() {}
-  
+
   async scan(): Promise<void> {
-    const result = await CapacitorBarcodeScanner.scanBarcode({
-      hint: CapacitorBarcodeScannerTypeHint.ALL
-    });
-    this.result = result.ScanResult;
+    try {
+      
+      const image = await Camera.getPhoto({
+        quality: 90,
+        allowEditing: false,
+        resultType: CameraResultType.DataUrl 
+      });
+
+      const img = new Image();
+      img.src = image.dataUrl!;
+      img.onload = () => {
+        const canvas = document.createElement('canvas');
+        const context = canvas.getContext('2d');
+        if (!context) {
+          this.result = 'Error: Unable to process the image.';
+          return;
+        }
+
+        canvas.width = img.width;
+        canvas.height = img.height;
+        context.drawImage(img, 0, 0);
+
+        
+        const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+        const code = jsQR(imageData.data, canvas.width, canvas.height);
+
+        if (code) {
+          this.result = code.data; 
+        } else {
+          this.result = 'No QR code detected';
+        }
+      };
+    } catch (error) {
+      console.error('Error capturing or processing QR code:', error);
+      this.result = 'Error scanning QR code';
+    }
   }
- 
 }
